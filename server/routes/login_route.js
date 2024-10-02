542f281cf598c23026420ecde3f72f29e7fc7cae
Jay Sabva <202101224@daiict.ac.in>
2024-10-02 22:52:29 +0530

    Add Send OTP Again feature + APIs for User Signup and Login.

--- server/routes/login_route.js
+++ server/routes/login_route.js
@@ -5,8 +5,6 @@ const loginController = require('../controllers/login_controll.js');
 
 router.post('/req-otp', loginController.requestOtp);
 router.post('/verify-otp', loginController.verifyOtp);
-router.post('/user-login',loginController.logIn)
-router.post('/user-signup', loginController.signUp);
 
 router.post('/signup', loginController.signup);
 router.post('/login', loginController.login);
