688e59c70bcabbc100cfcc666439506bc4ae57e3
Jay Sabva <202101224@daiict.ac.in>
2024-10-02 13:57:02 +0530

    Server Setup and MongoDB Connection

---
+++ server/index.js
@@ -0,0 +1,58 @@
+// Importing required modules
+const http = require('http');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const server = http.createServer(app);
+dotenv.config({path: './config/database_config.env'});
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Connecting to the database
+mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    console.log("Connected to the database");
+});
+
+// CORS middleware
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // * means all
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // * means all
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); // * means all
+        return res.status(200).json({});
+    }
+    next();
+});
+
+// Routes
+const loginRoute = require('./routes/login_route.js');
+app.use('/login', loginRoute);
+
+// Error Handling
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+// Starting the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
