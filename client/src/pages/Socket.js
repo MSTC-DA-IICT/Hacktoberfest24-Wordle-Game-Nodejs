3b77b2ad5da0118a6bf486afa75012c16d9a7aa4
Avinash <baraiyaavi05@gmail.com>
2024-10-05 17:21:59 +0530

    Update socket.io integration connected with backend and design

--- client/src/pages/Socket.js
+++ client/src/pages/Socket.js
@@ -1,11 +1,43 @@
-import React from 'react'
+// src/pages/SocketPage.js
+import React, { useEffect, useRef, useState } from 'react';
+import { io } from 'socket.io-client';
 
-function Socket() {
+function SocketPage() {
+  const socket = useRef();
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    socket.current = io("ws://localhost:5000");
+
+    socket.current.on("connection", () => {
+      console.log("connected to server");
+    });
+
+    return () => {
+      socket.current.disconnect();
+    };
+  }, []);
+
+  const handleClick = () => {
+    socket.current.emit("message", message);
+    setMessage(''); // Clear the input field after sending the message
+  };
+
   return (
-    <div>
-      hello
+    <div className="App flex flex-col justify-center items-center mt-10">
+      <p>Socket.io app</p>
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="Type your message here"
+        className="mb-4 p-2 border rounded mt-40 "
+      />
+      <button type="button mt-20" className="bg-slate-700 p-2 text-white rounded-sm" onClick={handleClick}>
+        Send
+      </button>
     </div>
-  )
+  );
 }
 
-export default Socket
+export default SocketPage;
